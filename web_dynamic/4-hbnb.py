79539eda412851a8f3fda1bbc5740bcacba868ff
binbashz <5732@holbertonstudents.com>
2023-09-28 21:54:00 +0000

    chmod +x all files

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""Starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid 
+import uuid
 
 
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/4-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -39,11 +39,11 @@ def hbnb():
 
     cache_id = str(uuid.uuid4())
 
-    return render_template('0-hbnb.html',
+    return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cahce_id=cache_id)
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
