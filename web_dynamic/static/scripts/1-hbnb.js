0bcb9de4e6e5f0e4e2fea71cb5f7fe36e153919d
binbashz <5732@holbertonstudents.com>
2023-09-29 20:52:01 +0000

    some fixs and semistandar

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,28 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  const amenities_h4 = document.querySelector('.amenities h4');
+$(document).ready(function () {
+  const selectedAmenityIds = {};
+  const selectedAmenityNames = {};
 
-  const amenities_list = [];
+  // Select all amenity input checkboxes
+  $('.amenities input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', (e) => {
-      if (e.target.checked === true) {
-        amenities_list.push(checkbox.getAttribute('data-name'));
-      } else {
-        amenities_list.forEach((name, idx) => {
-          if (name === checkbox.getAttribute('data-name')) {
-            amenities_list.splice(idx, 1);
-          }
-        });
-      }
-      amenities_h4.innerHTML = amenities_list;
-    });
+    // Check if the checkbox is checked or unchecked
+    if ($(this).is(':checked')) {
+      selectedAmenityIds[amenityId] = amenityName;
+      selectedAmenityNames[amenityName] = amenityId;
+    } else {
+      // Remove the amenity from the selectedAmenities object if unchecked
+      delete selectedAmenityIds[amenityId];
+      delete selectedAmenityNames[amenityName];
+    }
+
+    // Update the <h4> tag with the list of selected amenities
+    const selectedAmenitiesList = Object.keys(selectedAmenityIds).join(', ');
+
+    $('.amenities h4').text('Selected Amenities: ' + selectedAmenitiesList);
+
+    // Check the state of selectedAmenityIds
+    console.log('Selected Amenity IDs:', selectedAmenityIds);
   });
 });
