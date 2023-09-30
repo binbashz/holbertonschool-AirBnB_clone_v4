c47f442fbb62f9d691ca1b4f99cc6d8d048de75e
binbashz <5732@holbertonstudents.com>
2023-09-30 23:01:53 +0000

    Applying style corrections with semistandard

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,107 +1,93 @@
-// Based on ajax
-// When the button tag is clicked, a new POST request to places_search should be made with the list of Amenities checked
-
-// Wait for the DOM to be fully loaded before executing the script
 $(document).ready(function () {
-  // Select all checkboxes on the page
-  const checkboxes = $('input[type="checkbox"]');
-  // Select the <h4> element inside the element with class "amenities"
-  const amenities_h4 = $('.amenities h4');
-  // Select the element with the ID "api_status"
-  const apiStatusDiv = $('#api_status');
+  let selectedAmenities = []
 
-  // Initialize an empty list to store selected amenities
-  let amenities_list = [];
+  function checkApiHealth () {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:5001/api/v1/status/',
+      success: function (data) {
+        if (data.status === 'OK') {
+          $('#api_status').addClass('healthy')
+        } else {
+          $('#api_status').removeClass('healthy')
+        }
+      }
+    })
+  }
 
-  // Select the search button by its ID
-  const searchButton = $('#search-button');
+  checkApiHealth()
 
-  // Add a click event listener to the search button
-  searchButton.click(function () {
-    // Make a POST request to places_search with the list of Amenities checked
+  $('.amenities input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id')
+    const amenityName = $(this).data('name')
+
+    const existingAmenity = selectedAmenities.find(function (amenity) {
+      return amenity.id === amenityId
+    })
+
+    if ($(this).is(':checked')) {
+      if (!existingAmenity) {
+        selectedAmenities.push({ id: amenityId, name: amenityName })
+      }
+    } else {
+      selectedAmenities = selectedAmenities.filter(function (amenity) {
+        return amenity.id !== amenityId
+      })
+    }
+
+    const amenitiesList = selectedAmenities.map(function (amenity) {
+      return amenity.name
+    }).join(', ')
+
+    $('.amenities h4').text('Selected Amenities: ' + amenitiesList)
+
+    console.log('Selected Amenities:', selectedAmenities)
+  })
+
+  function createPlaceCard (place) {
+    const card = `
+      <article>
+        <div class="title-box">
+          <h2>${place.name}</h2>
+          <div class="price">${place.price_by_night}</div>
+        </div>
+        <div class="info">
+          <div class="guests">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+          <div class="rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+          <div class="bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+        </div>
+        <div class="description">
+          ${place.description}
+        </div>
+      </article>
+    `
+    return card
+  }
+
+  function sendSearchRequest () {
+    const amenityIds = selectedAmenities.map(function (amenity) {
+      return amenity.id
+    })
+
     $.ajax({
       type: 'POST',
-      url: '/api/v1/places_search',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
       contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({ amenities: amenities_list }),
+      data: JSON.stringify({ amenities: amenityIds }),
       success: function (data) {
-        // Handle the response data and update the places section as needed
-        // Example: Update the places section with the data returned from the API
-        $('.places').empty(); // Clear existing content
-        data.result.forEach(function (place) {
-          // Create a new article for each place and append it to the places section
-          const article = $('<article>');
-          article.html(`
-                        <div class="title">
-                            <h2>${place.name}</h2>
-                            <div class="price_by_night">
-                                ${place.price_by_night}
-                            </div>
-                        </div>
-                        <div class="information">
-                            <div class="max_guest">
-                                <i class="fa fa-users fa-3x" aria-hidden="true"></i><br>
-                                ${place.max_guest} Guests
-                            </div>
-                            <div class="number_rooms">
-                                <i class="fa fa-bed fa-3x" aria-hidden="true"></i><br>
-                                ${place.number_rooms} Bedrooms
-                            </div>
-                            <div class="number_bathrooms">
-                                <i class="fa fa-bath fa-3x" aria-hidden="true"></i><br>
-                                ${place.number_bathrooms} Bathrooms
-                            </div>
-                        </div>
-                        <div class="description">
-                            ${place.description}
-                        </div>
-                    `);
-          $('.places').append(article);
-        });
-      },
-      error: function (error) {
-        // Handle errors here
-        console.error('Error:', error);
+        $('.places').empty()
+        for (const place of data) {
+          const card = createPlaceCard(place)
+          $('.places').append(card)
+        }
+        console.log('Search successful')
       }
-    });
-  });
+    })
+  }
 
-  // Loop through each checkbox
-  checkboxes.each(function () {
-    // Add a change event listener to each checkbox
-    $(this).change(function () {
-      // Check if the checkbox is checked
-      if ($(this).is(':checked')) {
-        // If checked, add the amenity name to the list
-        amenities_list.push($(this).data('name'));
-      } else {
-        // If unchecked, remove the amenity name from the list
-        amenities_list = amenities_list.filter(function (name) {
-          return name !== $(this).data('name');
-        });
-      }
-      // Update the displayed amenities list with a comma-separated string
-      amenities_h4.text(amenities_list.join(', '));
-    });
-  });
+  sendSearchRequest()
 
-  // Initial API status check
-  $.ajax({
-    type: 'GET',
-    url: 'http://0.0.0.0:5001/api/v1/status/',
-    success: function (data) {
-      // Check if the API status is "OK" and update the class accordingly
-      if (data.status === 'OK') {
-        apiStatusDiv.addClass('available');
-      } else {
-        // If the API status is not "OK," remove the "available" class
-        apiStatusDiv.removeClass('available');
-      }
-    },
-    error: function (error) {
-      // Handle errors here
-      console.error('Error fetching API status:', error);
-    }
-  });
-});
+  $('#search-button').click(function () {
+    sendSearchRequest()
+  })
+})
