ff1ba09dbda485ac828e76d23453a4847e8a88c3
binbashz <5732@holbertonstudents.com>
2023-09-30 22:51:19 +0000

    standard --fix 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,85 +1,84 @@
-// wait for the DOM to be fully loaded before executing the script
-window.addEventListener('DOMContentLoaded', () => {
-  // Select all checkboxes on the page
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  // Select the <h4> element inside the element with class "amenities"
-  const amenities_h4 = document.querySelector('.amenities h4');
-  // Select the element with the ID "api_status"
-  const apiStatusDiv = document.querySelector('#api_status');
+$(document).ready(function () {
+  let selectedFeatures = []
 
-  // Initialize an empty list to store selected amenities
-  let amenities_list = [];
-
-  // Loop through each checkbox
-  checkboxes.forEach(checkbox => {
-    // Add a change event listener to each checkbox
-    checkbox.addEventListener('change', (e) => {
-      // Check if the checkbox is checked
-      if (e.target.checked === true) {
-        // If checked, add the amenity name to the list
-        amenities_list.push(checkbox.getAttribute('data-name'));
-      } else {
-        // If unchecked, remove the amenity name from the list
-        amenities_list = amenities_list.filter(name => name !== checkbox.getAttribute('data-name'));
-      }
-      // Update the displayed amenities list with a comma-separated string
-      amenities_h4.innerHTML = amenities_list.join(', ');
-
-      // Request the API status from the specified URL
-      fetch('http://0.0.0.0:5001/api/v1/status/')
-        .then(response => response.json())
-        .then(data => {
-          // Check if the API status is "OK" and update the class accordingly
-          if (data.status === 'OK') {
-            apiStatusDiv.classList.add('available');
-          } else {
-            // If the API status is not "OK," remove the "available" class
-            apiStatusDiv.classList.remove('available');
-          }
-        })
-        .catch(error => {
-          // Log an error message if there's an issue with the API request
-          console.error('Error fetching API status:', error);
-        });
-      $.ajax({
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        type: 'POST',
-        data: '{}',
-        contentType: 'application/json',
-        dataType: 'json',
-        success: function (data) {
-          $('SECTION.places').append(data.map(place => {
-            return `<ARTICLE>
-                                      <DIV class="title">
-                                        <H2>${place.name}</H2>
-                                        <DIV class="price_by_night">
-                                          ${place.price_by_night}
-                                        </DIV>
-                                      </DIV>
-                                      <DIV class="information">
-                                        <DIV class="max_guest">
-                                          <I class="fa fa-users fa-3x" aria-hidden="true"></I>
-                                          </BR>
-                                          ${place.max_guest} Guests
-                                        </DIV>
-                                        <DIV class="number_rooms">
-                                          <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
-                                          </BR>
-                                          ${place.number_rooms} Bedrooms
-                                        </DIV>
-                                        <DIV class="number_bathrooms">
-                                          <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
-                                          </BR>
-                                          ${place.number_bathrooms} Bathrooms
-                                        </DIV>
-                                      </DIV>
-                                      <DIV class="description">
-                                        ${place.description}
-                                      </DIV>
-                                    </ARTICLE>`;
-          }));
+  function checkApiStatus () {
+    $.ajax({
+      type: 'GET',
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      success: function (response) {
+        if (response.status === 'OK') {
+          $('#api_status').addClass('available')
+        } else {
+          $('#api_status').removeClass('available')
         }
-      });
-    });
-  });
-});
+      },
+      error: function (error) {
+        console.error('Error checking API status:', error)
+      }
+    })
+  }
+
+  checkApiStatus()
+
+  function createPlaceArticle (place) {
+    const articleHtml = `
+      <article>
+        <div class="place-title">
+          <h2>${place.name}</h2>
+          <div class="place-price">$${place.price_by_night}</div>
+        </div>
+        <div class="place-info">
+          <div class="place-max-guests">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+          <div class="place-bedrooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+          <div class="place-bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+        </div>
+        <div class="place-description">
+          ${place.description}
+        </div>
+      </article>
+    `
+    return articleHtml
+  }
+
+  function sendPostRequest () {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      success: function (placesData) {
+        for (const place of placesData) {
+          const placeArticle = createPlaceArticle(place)
+          $('.places').append(placeArticle)
+        }
+        console.log('Successfully fetched and displayed places.')
+      },
+      error: function (error) {
+        console.error('Error sending POST request:', error)
+      }
+    })
+  }
+
+  sendPostRequest()
+
+  $('.features input[type="checkbox"]').change(function () {
+    const featureId = $(this).data('id')
+    const featureName = $(this).data('name')
+
+    if ($(this).is(':checked')) {
+      selectedFeatures.push({ id: featureId, name: featureName })
+    } else {
+      selectedFeatures = selectedFeatures.filter(function (feature) {
+        return feature.id !== featureId
+      })
+    }
+
+    const featuresList = selectedFeatures.map(function (feature) {
+      return feature.name
+    }).join(', ')
+
+    $('.features h4').text('Selected Features: ' + featuresList)
+
+    console.log('Selected Features:', selectedFeatures)
+  })
+})
